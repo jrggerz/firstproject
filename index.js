30b9acb809e260e12c19085cd538499b571cdda5
jrggerz <jorge.ruizlopez0904@gmail.com>
2022-03-15 20:50:45 -0600

    primera version radio

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+//How to know in what ip im runing
+/*
+var ip = require("ip");
+console.dir ( ip.address() );
+*/
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/index.html'));
+
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
