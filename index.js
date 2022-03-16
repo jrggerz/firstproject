6e51062b684a10b49b3c6ee4aba876b1d4a13c7b
Jorge Ruiz <99931189+jrggerz@users.noreply.github.com>
2022-03-15 21:11:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const port = 3000;
 const path = require('path');
 
 //How to know in what ip im runing
-/*
+
 var ip = require("ip");
 console.dir ( ip.address() );
-*/
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/src/index.html'));
